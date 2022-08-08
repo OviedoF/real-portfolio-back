5dbbc0cf6c00a7211cc1d59703f10fe39aaae931
Oviedo F <oviedofederico039@gmail.com>
2022-08-07 21:12:02 -0300

    cv

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
     res.render(path.join(__dirname, '..', 'public', 'index.html'));
 })
 
+router.get('/cv', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'FEDERICO NAHUEL OVIEDO CV.pdf'))
+})
+
 module.exports = router;
